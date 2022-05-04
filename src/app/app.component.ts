62c73f2ff42ec9dd19f05ba2aefdb09d06c36f45
steelvelveteen <joeyvico@gmail.com>
2022-05-04 18:43:41 +0200

    changing tailwind to ngx-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-template';
-
-  sidenavToggle = () => console.log('Side Navbar to toggle now');
+  isCollapsed = true;
+  scrollToDownload(element: any) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MaterialModule } from './material.module';
+import { BootstrapModule } from './bootstrap.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MaterialModule,
+    BootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
