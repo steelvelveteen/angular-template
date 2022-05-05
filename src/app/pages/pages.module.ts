ee420dd1fedeb32813717dde90376a99feae90af
steelvelveteen <joeyvico@gmail.com>
2022-05-05 10:28:55 +0200

    Staged to get started with navbar. Created pages.module.ts and add a home component to it

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [CommonModule, BrowserModule, FormsModule, RouterModule],
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
+  providers: [],
+})
+export class PagesModule {}
