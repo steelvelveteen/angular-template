8b268f31d16634533eb994bed2b48de44ac06d10
steelvelveteen <joeyvico@gmail.com>
2022-05-12 09:16:44 +0200

    Created a login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertButtonComponent } from './alert-button/alert-button.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, AlertButtonComponent],
+  declarations: [AppComponent, AlertButtonComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
